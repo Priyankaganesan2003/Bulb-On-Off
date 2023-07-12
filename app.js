c647df36febebad018ae624d207f53a3e596f65d
Priyankaganesan2003 <122596756+Priyankaganesan2003@users.noreply.github.com>
2023-07-12 15:17:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_btn = document.getElementById("on");
+var off_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+
+function bulb_on() {
+    img.src="https://media.istockphoto.com/id/161767656/vector/glowing-yellow-incandescent-light-bulb.jpg?s=612x612&w=0&k=20&c=eWDoxaTIoZU1qNkoj4kPU-rDeFlWCGbfiM_IOYqLh9k=";
+}
+
+function bulb_off() {
+    img.src="https://c1.wallpaperflare.com/preview/825/807/809/bulb-electricity-energy-glass.jpg";
+}
